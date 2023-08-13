d535de7a7077510d8f02ac20b6ad3051c57a831e
walmonn <wagner.a.monte@hotmail.com>
2023-08-12 21:57:48 -0300

    commit_004

--- Mundo_1/ex004_dissecando_uma_variavel.py
+++ Mundo_1/ex004_dissecando_uma_variavel.py
@@ -1,3 +1,5 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ele.
+
 a = input("Digite alguma coisa: ")
 print("O tipo primitivo desse valor é ", type(a))
 print("Só tem espaços ?", a.isspace())

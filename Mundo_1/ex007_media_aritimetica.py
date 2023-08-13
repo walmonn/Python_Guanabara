e1d7dc1dc62d5ad024985db7eb44c4db07e16603
walmonn <wagner.a.monte@hotmail.com>
2023-08-12 21:31:37 -0300

    commit_007

--- Mundo_1/ex007_media_aritimetica.py
+++ Mundo_1/ex007_media_aritimetica.py
@@ -2,4 +2,4 @@
 n1 = float(input('Primeira nota do Aluno: '))
 n2 = float(input('Segunda nota do Aluno: '))
 m = (n1 + n2) / 2
-print('A média entre {} e {} é igual a {}.' .format(n1, n2, m))
+print('A média entre {} e {} é igual a {}.'.format(n1, n2, m))

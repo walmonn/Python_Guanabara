a4e96ca5f25ca5db90063a822ea0915db4e85cda
walmonn <wagner.a.monte@hotmail.com>
2023-08-24 19:08:56 -0300

    commit_011

--- Mundo_1/ex011_pintando_parede.py
+++ Mundo_1/ex011_pintando_parede.py
@@ -6,4 +6,3 @@ area = larg * alt
 print('Sua parede tem a dimensão de {} x {} e sua área é de {}m²'.format(larg, alt, area))
 tinta = area / 2
 print('Para pintar essa parede, você precisará de {}L de tinta'.format(tinta))
-

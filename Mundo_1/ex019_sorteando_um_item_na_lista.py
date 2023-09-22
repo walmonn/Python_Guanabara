969143c91635ade2c221fb0e328cb1d8123ee19e
walmonn <wagner.a.monte@hotmail.com>
2023-09-22 14:31:13 -0300

    commit_019

--- Mundo_1/ex019_sorteando_um_item_na_lista.py
+++ Mundo_1/ex019_sorteando_um_item_na_lista.py
@@ -1,4 +1,5 @@
-# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+'''Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.'''
 
 import random
 

25c1f465b948649c6084911d3c96f381dbb63928
walmonn <wagner.a.monte@hotmail.com>
2024-10-01 22:28:18 -0300

    commit_teste

--- Mundo_1/ex022_analisador_de_textos.py
+++ Mundo_1/ex022_analisador_de_textos.py
@@ -13,4 +13,5 @@ print("Seu nome em minusculas é {}".format(nome.lower()))
 print("Seu nome ao todo tem {} letras".format(len(nome) - nome.count(" "))) 
 print("Seu primeiro nome tem {} letras".format(nome.find(" ")))
 separa = nome.split()
-print("Seu Primeiro nome é {} e tem {} letras".format(separa[0], len(separa[0])))
+print("Seu Primeiro nome é o que {} e tem {} letras".format(separa[0], len(separa[0])))
+
